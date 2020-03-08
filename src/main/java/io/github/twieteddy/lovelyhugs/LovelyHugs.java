98ee7ae5a52afd079c75bf906fad89e240d4e134
Teddy <twieteddy@gmail.com>
2020-03-08 11:45:46 +0100

    Remove unused code and apply google style

--- src/main/java/io/github/twieteddy/lovelyhugs/LovelyHugs.java
+++ src/main/java/io/github/twieteddy/lovelyhugs/LovelyHugs.java
@@ -3,22 +3,11 @@ package io.github.twieteddy.lovelyhugs;
 import io.github.twieteddy.lovelyhugs.commands.HugCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public class LovelyHugs extends JavaPlugin {
 
-    private static LovelyHugs instance = null;
-
-    @Override
-    public void onEnable() {
-        LovelyHugs.instance = this;
-        getCommand("hug").setExecutor(new HugCommand());
-    }
-
-    @Override
-    public void onDisable() {
-        LovelyHugs.instance = null;
-    }
-
-    public static LovelyHugs getInstance() {
-        return instance;
-    }
+  @Override
+  public void onEnable() {
+    getCommand("hug").setExecutor(new HugCommand());
+  }
 }
